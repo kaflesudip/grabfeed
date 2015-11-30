f9fbdd2bcc24f6e386982fa14c9bfb7808cacbcc
sudip <soodip.kafle@gmail.com>
2015-11-30 16:26:24 +0545

    0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name='grabfeed',
     packages=['grabfeed'],
-    version='0.3',
+    version='0.3.1',
     description='Detects and return RSS feeds for a given website.',
     author='Sudip Kafle',
     author_email='soodip.kafle@gmail.com',
     url='https://github.com/kaflesudip/grabfeed',
-    download_url='https://github.com/kaflesudip/grabfeed/tarball/0.3',
+    download_url='https://github.com/kaflesudip/grabfeed/tarball/0.3.1',
     keywords=['RSS', 'Feeds', 'Scraping'],
     install_requires=[
         'beautifulsoup4==4.4.1',
